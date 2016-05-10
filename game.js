4bfe3f3878115dd6cb5ef686b7c6d8be7407f8b1
Kevin Hinterlong <kevinhinterlong@gmail.com>
2016-05-09 22:42:58 -0500

    added animation of dice and statistics

--- game.js
+++ game.js
@@ -19,6 +19,23 @@ function setButtonListener(onClickFunction, buttonText) {
     submitButton.onclick = onClickFunction;
 }
 
+function printStats() {
+    var canvas = document.getElementById("statsCanvas"); 
+    var context = canvas.getContext("2d");
+    var initialHeight =20;
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.fillText("STATISTICS",110,20);
+    var lineSize = 20;
+    var stats = new Array();
+    stats.push("Games Played: " + gamesPlayed);
+    stats.push("Games Won: " + wins);
+    stats.push("Games Lost: " + losses);
+    for(var i=0; i < stats.length; ++i) {
+	context.fillText(stats[i],10, lineSize*(i+1)+initialHeight);
+    }
+
+}   
+
 function printInstructions() {
     var canvas = document.getElementById("instructionsCanvas");
     var context = canvas.getContext("2d");
@@ -98,6 +115,7 @@ function resetGame() {
 
 function gameManager() {
     resetGame();
+    printStats();
     gamesToPlay = parseInt(gameForm.elements["gamesToPlay"].value);
     if(isNaN(gamesToPlay) || gamesToPlay <1) {
 	return;
@@ -125,11 +143,82 @@ function setGamesToPlay() {
     gamesToPlay = gameForm.elements["gamesToPlay"].value = gamesToPlay;
 }
 
+function getShift(dice) {
+    switch(dice) {
+	case 0:
+	    return 40;
+	    break;
+	case 1:
+	    return 200;
+	    break;
+    }
+    return 0;
+}
+
+function getCircles(value) {
+    var circles;
+    switch(value) {
+	case 1:
+	    circles = [ [0,0,0],[0,1,0],[0,0,0] ];
+	    break;
+	case 2:
+	    circles = [ [1,0,0],[0,0,0],[0,0,1] ];
+	    break;
+	case 3:
+	    circles = [ [1,0,0],[0,1,0],[0,0,1] ];
+	    break;
+	case 4:
+	    circles = [ [1,0,1],[0,0,0],[1,0,1] ];
+	    break;
+	case 5:
+	    circles = [ [1,0,1],[0,1,0],[1,0,1] ];
+	    break;
+	case 6:
+	    circles = [ [1,0,1],[1,0,1],[1,0,1] ];
+	    break;
+    }
+    return circles;
+}
+
+function drawDice(dice,value,context) {
+    var verticalShift = 80;
+    var radius = 5;
+    var color = dice == 0 ? "blue" : "red";
+    var horizontalShift = getShift(dice);
+    var circlesToDraw = getCircles(value);
+    var diceSize = 120;
+    var spaceFromEdge = 15;
+    var spacingBetweenDots = (diceSize-2*spaceFromEdge)/2;
+    context.fillStyle = color;
+    context.fillRect(horizontalShift,verticalShift,diceSize,diceSize); 
+    context.fillStyle = "white";
+    for(var i=0; i < circlesToDraw.length; ++i) {
+	for(var j=0; j < circlesToDraw[i].length; ++j) {
+	    if(circlesToDraw[j][i] == 1) {
+		context.beginPath();
+		context.arc(horizontalShift + spaceFromEdge + spacingBetweenDots * i, verticalShift + spaceFromEdge + spacingBetweenDots * j, radius,0, 2*Math.PI );
+		context.fill();
+	    }
+	}
+    }
+}
+
+function animateDie() {
+    //clear canvas
+    var canvas = document.getElementById("gameCanvas");
+    var context = canvas.getContext("2d");
+    context.clearRect(0,0,canvas.width, canvas.height);
+    drawDice(0,dice1,context);
+    drawDice(1,dice2,context);
+}
+
 function waitForUserInput() {
     rollDice();
     updateValues();
+    animateDie();
     var finished = hasResults();
     if(finished) { //next game
+	printStats();
 	turn=0; //this modifies the behavior
 	gamesToPlay--;
 	setGamesToPlay();
@@ -156,9 +245,7 @@ function submitScores() {
 	    result = xhttp.responseText;
 	    if(result === "success") {
 		alert("Scores saved");
-	    } else {
-		alert(result);
-	    }
+	    } 
 	}
     };
     xhttp.open("POST", "scores.php", true);
